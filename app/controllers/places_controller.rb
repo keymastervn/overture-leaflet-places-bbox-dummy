391b16fd63ab262dff06bf7a15da9c52c61202ca
Dat Le-Tien <dat.le@employmenthero.com>
2024-11-22 16:57:59 +0700

    Display center marker only

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,12 +2,18 @@
 
 class PlacesController < ApplicationController
   def index
-    sw_lat = params[:sw_lat].to_f
-    sw_lng = params[:sw_lng].to_f
-    ne_lat = params[:ne_lat].to_f
-    ne_lng = params[:ne_lng].to_f
+    center_lat = params[:center_lat].to_f
+    center_lng = params[:center_lng].to_f
+    radius = params[:radius].to_f || 5000 # meters
 
-    @places = Place.where(latitude: sw_lat..ne_lat, longitude: sw_lng..ne_lng).limit(500)
+    # Ensure geographic calculations use the correct format (SRID 4326)
+    center_point = "SRID=4326;POINT(#{center_lng} #{center_lat})"
+
+    @places = Place.where(
+      "ST_DWithin(geopoint, ST_GeogFromText(?), ?)",
+      center_point,
+      radius
+    ).limit(500)
 
     # Render the places as JSON
     render json: @places.to_json(only: [:id, :name, :latitude, :longitude, :addresses])
