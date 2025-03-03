69b7435315e483dc7a07f7bd846cb50e502ed23e
Huy Nguyen Tan <huy.nguyen2@employmenthero.com>
2025-03-03 16:35:18 +0700

    Narrow down food and drink sub-categories (google) due to maximum 50

--- app/models/place.rb
+++ app/models/place.rb
@@ -67,57 +67,22 @@ class Place < ApplicationRecord
     # https://chatgpt.com/share/67878e3c-898c-800f-923e-4fb09b69b3a1
     'Food and Drink' => {
       'google' => %w[
-        american_restaurant
+        acai_shop
+        bagel_shop
         bakery
         bar
+        bar_and_grill
         barbecue_restaurant
-        brazilian_restaurant
         breakfast_restaurant
         brunch_restaurant
-        cafe
-        chinese_restaurant
-        coffee_shop
-        fast_food_restaurant
-        french_restaurant
-        greek_restaurant
-        hamburger_restaurant
-        ice_cream_shop
-        indian_restaurant
-        indonesian_restaurant
-        italian_restaurant
-        japanese_restaurant
-        korean_restaurant
-        lebanese_restaurant
-        meal_delivery
-        meal_takeaway
-        mediterranean_restaurant
-        mexican_restaurant
-        middle_eastern_restaurant
-        pizza_restaurant
-        ramen_restaurant
-        restaurant
-        sandwich_shop
-        seafood_restaurant
-        spanish_restaurant
-        steak_house
-        sushi_restaurant
-        thai_restaurant
-        turkish_restaurant
-        vegan_restaurant
-        vegetarian_restaurant
-        vietnamese_restaurant
-        acai_shop
-        afghani_restaurant
-        african_restaurant
-        asian_restaurant
-        bagel_shop
-        bar_and_grill
         buffet_restaurant
+        cafe
         cafeteria
         candy_store
         cat_cafe
         chocolate_factory
         chocolate_shop
+        coffee_shop
         confectionery
         deli
         dessert_restaurant
@@ -125,14 +90,26 @@ class Place < ApplicationRecord
         diner
         dog_cafe
         donut_shop
+        fast_food_restaurant
         fine_dining_restaurant
         food_court
+        hamburger_restaurant
+        ice_cream_shop
         juice_shop
-        korean_restaurant
+        meal_delivery
+        meal_takeaway
+        pizza_restaurant
         pub
-        tea_house
-        wine_bar
+        ramen_restaurant
+        restaurant
         sandwich_shop
+        seafood_restaurant
+        steak_house
+        sushi_restaurant
+        tea_house
+        vegan_restaurant
+        vegetarian_restaurant
+        wine_bar
       ],
       'overture' => %w[
         eat_and_drink
